bf4c24ce4ef005fb989da2f963e96a0622dc70fd
KangSuzy <astnwl321@gmail.com>
2022-05-30 16:56:26 +0900

    두 변수의 값 교환하기

---
+++ src/variable/VarEx2.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =20 ,y = 10;
+		int tmp = 0; // 두 변수의 값을 교환하기위해
+		
+		tmp = x;
+		x = y;
+		y = tmp;
+		
+		System.out.println("x:"+x+" y:" +y);
+	}
+
+}
