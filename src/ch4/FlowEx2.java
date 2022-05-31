acb6fe7d32e2b6eba8cc5dce01a9fab41cfbc6e5
KangSuzy <astnwl321@gmail.com>
2022-05-31 13:45:07 +0900

    ch4 if 조건식예제

---
+++ src/ch4/FlowEx2.java
@@ -0,0 +1,25 @@
+package ch4;
+
+import java.util.*; // Scanner 클래스를 사용하기 위해 추가
+
+public class FlowEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input;
+		
+		System.out.print("숫자를 입력하세요 : ");
+		
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine(); // 화면을 통해 입력받은 내용을 tmp에 저장
+		input = Integer.parseInt(tmp); // 입력받은 문자열(tmp)를 숫자로 변환
+		
+		if (input==0) {
+			System.out.println("입력하신 숫자는 0 입니다");
+		}
+		if (input!=0) {
+			System.out.printf("입력하신 숫자는 %d 입니다", input);
+		}
+	} // main 끝
+
+}
